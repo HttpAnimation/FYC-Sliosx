5bb39681e7093c064a1f5c2b0ba72f09ff4a2b4b
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-01-23 09:30:52 -0700

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-index-html","displayName":"index.html","link":{"linkType":"LinkTypePage","href":"#!page-index-html"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-main-html","displayName":"main.html","link":{"linkType":"LinkTypePage","href":"#!page-main-html"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-index-html","displayName":"index.html","link":{"linkType":"LinkTypePage","href":"#!page-index-html"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-main-html","displayName":"main.html","link":{"linkType":"LinkTypePage","href":"#!page-main-html"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
